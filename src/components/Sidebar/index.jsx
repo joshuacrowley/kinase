8caaad054964d82b33d31a6c42cdf8291fad408a
Joshua Crowley <joshua.crowley@gmail.com>
2017-11-30 14:37:45 +1100

    Revert "Added <p> test"
    This reverts commit 3d73415b355c1b4432cb4e83d14a884dd643bc9d.

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,38 +1,36 @@
-import mapValues from "lodash.mapvalues";
-import sortBy from "lodash.sortby";
-import values from "lodash.values";
-import MdSave from "react-icons/lib/md/save";
-import PropTypes from "prop-types";
-import React from "react";
-import Spinner from "react-spinkit";
-import { connect } from "react-redux";
-import SearchInput, { createFilter } from "react-search-input";
+import mapValues from 'lodash.mapvalues';
+import sortBy from 'lodash.sortby';
+import values from 'lodash.values';
+import MdSave from 'react-icons/lib/md/save';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Spinner from 'react-spinkit';
+import { connect } from 'react-redux';
+import SearchInput, {
+  createFilter,
+} from 'react-search-input';
 
-import manifest from "manifest.json";
-import AnnotatedItem from "components/AnnotatedItem";
-import { saveAnnotatedItems } from "redux/proxyActions";
-import { annotationContextType } from "redux/reducers/annotationContext";
-import { currentContextSelector } from "redux/selectors";
+import manifest from 'manifest.json';
+import AnnotatedItem from 'components/AnnotatedItem';
+import { saveAnnotatedItems } from 'redux/proxyActions';
+import { annotationContextType } from 'redux/reducers/annotationContext';
+import { currentContextSelector } from 'redux/selectors';
 
-import "sweetalert2/dist/sweetalert2.css";
-import styles from "./style.scss";
+import 'sweetalert2/dist/sweetalert2.css';
+import styles from './style.scss';
 
 class Sidebar extends React.Component {
   state = {
-    search: ""
+    search: '',
   };
 
   getAnnotations() {
-    const annotations = values(
-      mapValues(this.props.context, (annotation, name) => ({
-        ...annotation,
-        name
-      }))
-    );
+    const annotations = values(mapValues(this.props.context, (annotation, name) => ({
+      ...annotation,
+      name,
+    })));
     const filteredAnnotations = sortBy(
-      annotations.filter(createFilter(this.state.search, "name")),
-      "name"
-    );
+      annotations.filter(createFilter(this.state.search, 'name')), 'name');
     return filteredAnnotations.map(annotation => (
       <li key={annotation.name}>
         <AnnotatedItem annotationName={annotation.name} />
@@ -40,36 +38,25 @@ class Sidebar extends React.Component {
     ));
   }
 
-  searchUpdated = search => {
+  searchUpdated = (search) => {
     this.setState({ search });
-  };
+  }
 
   render() {
     return (
       <div className={styles.sidebar}>
         <header>
-          <span>{manifest.name}</span>
-          <span
-            className={styles.saveIndicator}
-            onClick={this.props.saveAnnotatedItems}
-          >
-            {this.props.saving ? (
-              <Spinner
-                className={styles.saveSpinner}
-                noFadeIn
-                spinnerName={"three-bounce"}
-              />
-            ) : (
-              <MdSave className={styles.saveButton} />
-            )}
+          <span>{ manifest.name }</span>
+          <span className={styles.saveIndicator} onClick={this.props.saveAnnotatedItems}>
+            {
+              this.props.saving
+              ? <Spinner className={styles.saveSpinner} noFadeIn spinnerName={'three-bounce'} />
+              : <MdSave className={styles.saveButton} />
+            }
           </span>
         </header>
-        <SearchInput
-          className={styles.searchBox}
-          onChange={this.searchUpdated}
-        />
+        <SearchInput className={styles.searchBox} onChange={this.searchUpdated} />
         <ul className={styles.annotationList}>{this.getAnnotations()}</ul>
-        <p>Josh</p>
       </div>
     );
   }
@@ -78,15 +65,15 @@ class Sidebar extends React.Component {
 Sidebar.propTypes = {
   saveAnnotatedItems: PropTypes.func.isRequired,
   context: annotationContextType.isRequired,
-  saving: PropTypes.bool.isRequired
+  saving: PropTypes.bool.isRequired,
 };
 
 export default connect(
   state => ({
     context: currentContextSelector(state),
-    saving: state.saving
+    saving: state.saving,
   }),
   dispatch => ({
-    saveAnnotatedItems: () => dispatch(saveAnnotatedItems())
-  })
+    saveAnnotatedItems: () => dispatch(saveAnnotatedItems()),
+  }),
 )(Sidebar);
